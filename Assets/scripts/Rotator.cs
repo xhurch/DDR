1d47d1aa1ec7b2a08c6158fc18d18b292fd38d18
PIMG <info@xhurch.net>
2017-03-25 00:02:06 -0700

    this is the project

---
+++ Assets/scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+		void Update () 
+		{
+				transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+		}
+
+		void Awake() {
+				AudioSource[] sources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+				foreach (AudioSource source in sources) {
+						Debug.Log(source.gameObject.name);
+				}
+
+		}
+}
