8ac75af9eb3b9ff1b749eb56450fe01245fe0efd
Brad Anderson Smith <bradsmithinc@gmail.com>
2017-03-25 19:23:54 -0800

    Wire Midi input to the particle effects

---
+++ Assets/scripts/GameTally.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using MidiJack;
+using System.Collections;
+
+public class Tally {
+
+	public int count;
+	public int note;
+	public MidiChannel channel;
+
+	public Tally()
+	{
+		count = 0;
+		note = 0;
+		channel = MidiChannel.Ch1;
+	}
+
+	public void Increment(){
+
+		count++;
+
+		//Debug.Log ("Note " + note + ": " + count);
+	}
+
+	public void Reset(){
+
+		count = 0;
+	}
+}
+
+public class GameTally : MonoBehaviour
+{
+
+	public Tally[] emojiTally;
+
+	GameTally(){
+
+		emojiTally = new Tally[6];
+
+		for (int i = 0; i < 6; i++) {
+			emojiTally [i] = new Tally ();
+		}
+
+		emojiTally[0].note = 35;
+		emojiTally[1].note = 36;
+		emojiTally[2].note = 37;
+		emojiTally[3].note = 38;
+		emojiTally[4].note = 39;
+		emojiTally[5].note = 40;
+	}
+
+	void NoteOn(MidiChannel channel, int note, float velocity)
+	{
+		Debug.Log("NoteOn: " + channel + "," + note + "," + velocity);
+
+		for (int i = 0; i < 6; i++) {
+			if (emojiTally [i].note == note) {
+				emojiTally [i].Increment ();
+
+			}
+		}
+	}
+
+	void Reset(){
+
+		for (int i = 0; i < 6; i++) {
+			emojiTally [i].Reset ();
+		}
+	}
+
+	void OnEnable()
+	{
+		MidiMaster.noteOnDelegate += NoteOn;
+	}
+
+	void OnDisable()
+	{
+		MidiMaster.noteOnDelegate -= NoteOn;
+	}
+}
---
+++ Assets/scripts/LightControls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightControls : MonoBehaviour {
+
+	//SerialControl serialControl;
+	private int lightMode;
+	private float lightLevel;
+
+	// Use this for initialization
+	void Start () {
+
+		lightMode = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			Trigger();
+		}
+			
+		if (lightMode == 0) {
+
+			lightLevel -= 0.8f * Time.deltaTime;
+
+			if (lightLevel <= 0.0f)
+				lightLevel = 0.0f;
+		
+		} else {
+
+			lightLevel += 1.2f * Time.deltaTime;
+
+			if (lightLevel >= 1.0f) {
+
+				lightMode = 0;
+			}
+		}
+
+		//Debug.Log("ll: "+lightLevel);
+	}
+
+	void Trigger(){
+
+		lightMode = 1;
+	}
+}
